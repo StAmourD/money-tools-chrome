b5bfd26bf94dc3a0d398ba4344e8a98bb4d46bbc
David St.Amour <stamourd@hotmail.com>
2018-04-15 14:39:51 -0400

    basic button functionality added

--- main.js
+++ main.js
@@ -20,6 +20,17 @@ var rowselect = class {
     this.down();
     // insert input element to copy to clipboard from
     $().add('<input type="text" value="" id="toCopy">').appendTo($(document).xpathEvaluate('//*[@id="body_rightcolumn"]/div[6]'));
+    // insert table header "//*[@id="body_rightcolumn"]/div[6]/table/tbody/tr[1]"
+    $().add('<th>MS Money</th>').appendTo($(document).xpathEvaluate('//*[@id="body_rightcolumn"]/div[6]/table/tbody/tr[1]'));
+    // add buttons to rows
+    $().add('\
+      <td class="GridField" style="background-color: #66A182;">\
+        <div class="a_btn a_btn-one">\
+          <span id="tRow-1" class="tRow">Add</span>\
+        </div>\
+      </td>\
+    ').appendTo($(document).xpathEvaluate('//*[@id="body_rightcolumn"]/div[6]/table/tbody/tr[2]'));
+
   }
   down() {
     if (this.i < this.max) {
@@ -64,11 +75,93 @@ var rowselect = class {
   }
 };
 
+var injectCss = function() {
+  $("<style>").prop("type", "text/css").html("\
+    .a_btn {\
+      line-height: 20px;\
+      height: 20px;\
+      text-align: center;\
+      width: 50px;\
+      cursor: pointer;\
+    }\
+    \
+    .a_btn-one {\
+      color: #FFF;\
+      transition: all 0.3s;\
+      position: relative;\
+    }\
+    \
+    .a_btn-one span {\
+      transition: all 0.3s;\
+    }\
+    \
+    .a_btn-one::before {\
+      content: '';\
+      position: absolute;\
+      bottom: 0;\
+      left: 0;\
+      width: 100%;\
+      height: 100%;\
+      z-index: 1;\
+      opacity: 0;\
+      transition: all 0.3s;\
+      border-top-width: 1px;\
+      border-bottom-width: 1px;\
+      border-top-style: solid;\
+      border-bottom-style: solid;\
+      border-top-color: rgba(255, 255, 255, 0.5);\
+      border-bottom-color: rgba(255, 255, 255, 0.5);\
+      transform: scale(0.1, 1);\
+    }\
+    \
+    .a_btn-one:hover span {\
+      letter-spacing: 2px;\
+    }\
+    \
+    .a_btn-one:hover::before {\
+      opacity: 1;\
+      transform: scale(1, 1);\
+    }\
+    \
+    .a_btn-one::after {\
+      content: '';\
+      position: absolute;\
+      bottom: 0;\
+      left: 0;\
+      width: 100%;\
+      height: 100%;\
+      z-index: 1;\
+      transition: all 0.3s;\
+      background-color: rgba(255, 255, 255, 0.1);\
+    }\
+    \
+    .a_btn-one:hover::after {\
+      opacity: 0;\
+      transform: scale(0.1, 1);\
+    }\
+    ").appendTo("head");
+}
+
+var addClickListeners = function() {
+  var btns = document.getElementsByClassName('a_btn');
+  btns['0'].addEventListener("click", copyToClipboard, true);
+}
+
+var copyToClipboard = function() {
+  var elID = this.firstElementChild.getAttribute("id");
+  var idArr = elID.split('-')
+  var thisID = idArr[idArr.length - 1]
+
+  alert(thisID);
+};
+
 // on DOM load
 $(function() {
   "use strict";
 
   var rowSelect = new rowselect();
+  injectCss();
+  addClickListeners();
 
   window.addEventListener("keydown", function(event) {
     if (event.defaultPrevented) {
@@ -90,6 +183,5 @@ $(function() {
           break;
       }
     }
-
   }, true);
 });
